5d76a9ed6c1eafb90e0818d1a6b48cf8aebe86b4
Aakash Desai <adesai@mozilla.com>
2009-06-24 16:04:33 +0200

    Bug 493787: Restore home page to default, r=hskupin

---
+++ firefox/testPreferences/testRestoreHomepageToDefault.js
@@ -0,0 +1,117 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Mozmill Test Code.
+ *
+ * The Initial Developer of the Original Code is Mozilla Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Aakash Desai <adesai@mozilla.com>
+ *   Henrik Skupin <hskupin@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+// Include necessary modules
+var RELATIVE_ROOT = '../../shared-modules';
+var MODULE_REQUIRES = ['PrefsAPI', 'UtilsAPI'];
+
+const gDelay = 0;
+
+var setupModule = function(module) {
+  module.controller = mozmill.getBrowserController();
+}
+
+/**
+ *  Testcase ID #6287 - Restore Home Page to default
+ */
+var testRestoreHomeToDefault = function() {
+  // Close all tabs and open a blank page
+  UtilsAPI.closeAllTabs(controller);
+
+  // Open a web page for the temporary home page
+  controller.open('http://www.mozilla.org/');
+  controller.waitForPageLoad(controller.tabs.activeTab);
+
+  var link = new elementslib.Link(controller.tabs.activeTab, "Mozilla");
+  controller.assertNode(link);
+
+  // Call Preferences dialog and set home page
+  PrefsAPI.handlePreferencesDialog(prefDialogHomePageCallback);
+
+  // Go to the saved home page and verify it's the correct page
+  controller.click(new elementslib.ID(controller.window.document, "home-button"));
+  controller.waitForPageLoad(controller.tabs.activeTab);
+  controller.assertNode(link);
+
+  // Open Preferences dialog and reset home page to default
+  PrefsAPI.handlePreferencesDialog(prefDialogDefHomePageCallback);
+}
+
+var prefDialogHomePageCallback = function(controller) {
+  // Select the Main pane
+  controller.click(new elementslib.Lookup(controller.window.document, '/id("BrowserPreferences")/anon({"orient":"vertical"})/anon({"anonid":"selector"})/{"pane":"paneMain"}'));
+
+  // Check if the main pane is active
+  var node = new elementslib.ID(controller.window.document, 'paneMain');
+  UtilsAPI.delayedAssertNode(controller, node);
+  controller.sleep(gDelay);
+
+  // Set home page to the current page
+  controller.click(new elementslib.ID(controller.window.document, "useCurrent"));
+  controller.sleep(gDelay);
+
+  // Close the Preferences dialog
+  if (mozmill.isWindows) {
+    var okButton = new elementslib.Lookup(controller.window.document, '/id("BrowserPreferences")/anon({"anonid":"dlg-buttons"})/{"dlgtype":"accept","icon":"accept","default":"false"}')
+    controller.click(okButton);
+  } else {
+    controller.keypress(null, 'VK_ESCAPE', {});
+  }
+}
+
+var prefDialogDefHomePageCallback = function(controller) {
+  // Main pane should still be visible. Just click to be on the safe side
+  controller.click(new elementslib.Lookup(controller.window.document, '/id("BrowserPreferences")/anon({"orient":"vertical"})/anon({"anonid":"selector"})/{"pane":"paneMain"}'));
+  controller.sleep(gDelay);
+
+  // Reset home page to the default page
+  var browserHomePage = new elementslib.ID(controller.window.document, "browserHomePage");
+  controller.click(new elementslib.ID(controller.window.document, "restoreDefaultHomePage"));
+  controller.sleep(gDelay);
+
+  // Check the browserconfig file to get the get default homepage
+  var defaultHomePage = UtilsAPI.getProperty("resource:/browserconfig.properties", "browser.startup.homepage");
+  controller.assertValue(browserHomePage, defaultHomePage);
+
+    // Close the Preferences dialog
+  if (mozmill.isWindows) {
+    var okButton = new elementslib.Lookup(controller.window.document, '/id("BrowserPreferences")/anon({"anonid":"dlg-buttons"})/{"dlgtype":"accept","icon":"accept","default":"false"}')
+    controller.click(okButton);
+  } else {
+    controller.keypress(null, 'VK_ESCAPE', {});
+  }
+}
--- shared-modules/testUtilsAPI.js
+++ shared-modules/testUtilsAPI.js
@@ -56,6 +56,7 @@ var closeAllTabs = function(controller) {
   }
 
   controller.open("about:blank");
+  controller.waitForPageLoad(controller.tabs.activeTab);
 }
 
 /**
