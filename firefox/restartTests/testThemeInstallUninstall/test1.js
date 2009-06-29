0c75acbeea0bc3e792a2c8cbbbbf7c6800c5e8a8
Henrik Skupin <hskupin@mozilla.com>
2009-06-29 21:17:21 +0200

    Bug 501105: Installing a theme

---
+++ firefox/restartTests/testThemeInstallUninstall/test1.js
@@ -0,0 +1,129 @@
+/* * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is MozMill Test code.
+ *
+ * The Initial Developer of the Original Code is Mozilla Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Henrik Skupin <hskupin@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * **** END LICENSE BLOCK ***** */
+
+// Include necessary modules
+var RELATIVE_ROOT = '../../../shared-modules';
+var MODULE_REQUIRES = ['ModalDialogAPI', 'UtilsAPI'];
+
+// Shared variable
+var gThemeName = "Walnut for Firefox";
+
+var setupModule = function(module) {
+  module.controller = mozmill.getBrowserController();
+  module.addonsController = mozmill.getAddonsController();
+}
+
+var teardownModule = function(module) {
+  // Close all open tabs
+  UtilsAPI.closeAllTabs(controller);
+}
+
+/**
+ *  Testcase ID #5930 - Install a theme
+ */
+var testInstallTheme = function() {
+  // Make sure only one tab is open
+  UtilsAPI.closeAllTabs(controller);
+
+  // Make sure the Get Add-ons pane is visible
+  var getAddonsPane = new elementslib.ID(addonsController.window.document, "search-view");
+  UtilsAPI.delayedClick(addonsController, getAddonsPane);
+
+  // Wait for the Browse All Add-ons link and click on it
+  var browseAddonsLink = new elementslib.ID(addonsController.window.document, "browseAddons");
+  UtilsAPI.delayedClick(addonsController, browseAddonsLink);
+
+  // The target web page is loaded lazily so wait for the newly created tab first
+  controller.waitForEval("subject.length == 2", 5000, 100, controller.tabs);
+  controller.waitForPageLoad(controller.tabs.activeTab);
+
+  // Open the web page for the Walnut theme directly
+  controller.open("https://addons.mozilla.org/en-US/firefox/addon/122");
+  controller.waitForPageLoad(controller.tabs.activeTab);
+
+  // Create a modal dialog instance to handle the Software Installation dialog
+  var md = new ModalDialogAPI.modalDialog(handleTriggerDialog);
+  md.start();
+
+  // Click link to install the theme which triggers a modal dialog
+  var triggerLink = new elementslib.XPath(controller.tabs.activeTab, "/html/body[@id='mozilla-com']/div/div[3]/div[@id='addon-summary']/div[1]/p[1]/a");
+  UtilsAPI.delayedClick(controller, triggerLink);
+
+  // Wait that the Installation pane is shown while the theme is installed
+  var installPane = new elementslib.ID(addonsController.window.document, "installs-view");
+  addonsController.waitForEval("subject.selected == true", 10000, 100, installPane.getNode());
+
+  // Check if the installed theme is visible in the Add-ons Manager
+  var theme = new elementslib.Lookup(addonsController.window.document, '/id("extensionsManager")/id("addonsMsg")/id("extensionsBox")/[1]/id("extensionsView")/[1]/anon({"flex":"1"})/[0]/[1]/{"class":"addon-name-version","xbl:inherits":"name, version=newVersion"}/anon({"class":"addonName","crop":"end","xbl:inherits":"value=name","value":"' + gThemeName + '"})');
+  UtilsAPI.delayedAssertNode(addonsController, theme, 5000, 100);
+
+  // Check if restart button is present
+  var restartButton = new elementslib.XPath(addonsController.window.document, "/*[name()='window' and namespace-uri()='http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul']/*[name()='notificationbox' and namespace-uri()='http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul'][1]/*[name()='notification' and namespace-uri()='http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul'][1]/*[name()='button' and namespace-uri()='http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul'][1]");
+  UtilsAPI.delayedAssertNode(addonsController, restartButton);
+}
+
+/**
+ * Handle the Software Installation dialog
+ */
+var handleTriggerDialog = function(controller) {
+  // Get list of themes which should be installed
+  var itemList = controller.window.document.getElementById("itemList");
+  UtilsAPI.delayedAssertNode(controller, new elementslib.Elem(controller.window.document, itemList));
+
+  // There should be one theme for installation
+  if (itemList.childNodes.length != 1) {
+    throw "Expected one theme for installation";
+  }
+
+  // Check if the correct theme name is shown
+  if (itemList.childNodes[0].name != gThemeName) {
+    throw "Visible theme name doesn't match target theme";
+  }
+
+  // Will the theme be installed from https://addons.mozilla.org/?
+  if (itemList.childNodes[0].url.indexOf("https://addons.mozilla.org/") == -1) {
+    throw "Theme location doesn't contain https://addons.mozilla.org/";
+  }
+
+  // Check if the Cancel button is present
+  var cancelButton = new elementslib.Lookup(controller.window.document, '/id("xpinstallConfirm")/anon({"anonid":"buttons"})/{"dlgtype":"cancel"}');
+  controller.assertNode(cancelButton);
+
+  // Wait for the install button is enabled before clicking on it
+  var installButton = new elementslib.Lookup(controller.window.document, '/id("xpinstallConfirm")/anon({"anonid":"buttons"})/{"dlgtype":"accept"}');
+  controller.waitForEval("subject.disabled != true", 5000, 100, installButton.getNode());
+  controller.click(installButton);
+}
---
+++ firefox/restartTests/testThemeInstallUninstall/test2.js
@@ -0,0 +1,64 @@
+/* * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is MozMill Test code.
+ *
+ * The Initial Developer of the Original Code is Mozilla Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Henrik Skupin <hskupin@mozilla.com>
+*
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * **** END LICENSE BLOCK ***** */
+
+// Include necessary modules
+var RELATIVE_ROOT = '../../../shared-modules';
+var MODULE_REQUIRES = ['UtilsAPI'];
+
+// Shared variable
+const gThemeName = "Walnut for Firefox";
+
+var setupModule = function(module) {
+  module.controller = mozmill.getBrowserController();
+
+  // The Add-ons Manager is not opened automatically as what happens for new extensions
+  module.addonsController = mozmill.getAddonsController();
+}
+
+/**
+ *  Testcase ID #5930 - Install a theme
+ */
+var testCheckInstalledTheme = function() {
+  // Select the Themes pane
+  var themesPane = new elementslib.ID(addonsController.window.document, "themes-view");
+  UtilsAPI.delayedClick(addonsController, themesPane);
+
+  // The installed theme should be the active theme in the list
+  // XXX: Use a hard-coded name to access the entry directly until we can pass the info
+  // between restart test files (bug 500987)
+  var theme = new elementslib.Lookup(addonsController.window.document, '/id("extensionsManager")/id("addonsMsg")/id("extensionsBox")/[1]/id("extensionsView")/anon({"current":"true"})/anon({"flex":"1"})/{"class":"addonTextBox"}/anon({"anonid":"addonNameVersion"})/anon({"class":"addonName","crop":"end","xbl:inherits":"value=name","value":"' + gThemeName + '"})');
+  UtilsAPI.delayedAssertNode(addonsController, theme);
+}
