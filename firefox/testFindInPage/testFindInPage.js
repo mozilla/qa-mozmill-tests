0bc0ef80988df38ec8d417f03e559843b2eddd12
Anthony Hughes <ahughes@mozilla.com>
2009-06-29 23:52:19 +0200

    Bug 500095: Find Bar functionality, r=hskupin

---
+++ firefox/testFindInPage/testFindInPage.js
@@ -0,0 +1,117 @@
+/* * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is MozMill Test code.
+ *
+ * The Initial Developer of the Original Code is Mozilla Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Anthony Hughes <ahughes@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * **** END LICENSE BLOCK ***** */
+
+/** This test script is for testing
+ *  Litmus Test Case #5927: Find in page
+ *  https://litmus.mozilla.org/show_test.cgi?id=5927
+ */
+
+var RELATIVE_ROOT = '../../shared-modules';
+var MODULE_REQUIRES = ['UtilsAPI'];
+
+const gDelay = 0;
+
+var setupModule = function(module) {
+  controller = mozmill.getBrowserController();
+}
+
+var teardownModule = function(module) {
+  try {
+     // Just press Ctrl/Cmd + F to select the whole search string
+    controller.keypress(null, "f", {accelKey:true});
+
+    // Clear search text from the text field
+    var findBarTextField = new elementslib.Lookup(controller.window.document, '/id("main-window")/id("browser-bottombox")/id("FindToolbar")/anon({"anonid":"findbar-container"})/anon({"anonid":"find-field-container"})/anon({"anonid":"findbar-textbox"})');
+    controller.keypress(findBarTextField, 'VK_DELETE', {});
+
+    // Make sure the find bar is closed by click the X button
+    controller.click(new elementslib.Lookup(controller.window.document, '/id("main-window")/id("browser-bottombox")/id("FindToolbar")/anon({"anonid":"findbar-container"})/anon({"anonid":"find-closebutton"})'));
+  } catch(e) {
+  }
+}
+
+/**
+ * Test find in page functionality
+ *
+ * @throws Searched string does not match Selected string!
+ */
+var testFindInPage = function() {
+  // Open a website
+  controller.open("http://www.mozilla.org");
+  controller.waitForPageLoad();
+
+  // Press Ctrl/Cmd + F to open the find bar
+  controller.keypress(null, "f", {accelKey:true});
+  controller.sleep(gDelay);
+
+  // Check that the find bar is visible
+  var findBar = new elementslib.Lookup(controller.window.document, '/id("main-window")/id("browser-bottombox")/id("FindToolbar")/anon({"anonid":"findbar-container"})');
+  UtilsAPI.delayedAssertNode(controller, findBar);
+
+  // Type "mozilla" into the find bar text field and press return to start the search
+  var findBarTextField = new elementslib.Lookup(controller.window.document, '/id("main-window")/id("browser-bottombox")/id("FindToolbar")/anon({"anonid":"findbar-container"})/anon({"anonid":"find-field-container"})/anon({"anonid":"findbar-textbox"})');
+  controller.type(findBarTextField, "mozilla");
+  controller.keypress(null, "VK_RETURN", {});
+  controller.sleep(gDelay);
+
+  // Check that some text on the page has been highlighted
+  // (Lower case because we aren't checking for Match Case option)
+  var searchedText = findBarTextField.getNode().value;
+  var selectedText = controller.tabs.activeTabWindow.getSelection();
+  if (selectedText.toString().toLowerCase() != searchedText) {
+    throw "Searched string does not match Selected string!"
+  }
+
+  // Click the next button and check the strings again
+  var findBarNextButton = new elementslib.Lookup(controller.window.document, '/id("main-window")/id("browser-bottombox")/id("FindToolbar")/anon({"anonid":"findbar-container"})/anon({"anonid":"find-next"})');
+  controller.click(findBarNextButton);
+  controller.sleep(gDelay);
+
+  selectedText = controller.tabs.activeTabWindow.getSelection();
+  if (selectedText.toString().toLowerCase() != searchedText) {
+    throw "Searched string does not match Selected string!"
+  }
+
+  // Click the prev button and check the strings again
+  var findBarPrevButton = new elementslib.Lookup(controller.window.document, '/id("main-window")/id("browser-bottombox")/id("FindToolbar")/anon({"anonid":"findbar-container"})/anon({"anonid":"find-previous"})');
+  controller.click(findBarPrevButton);
+  controller.sleep(gDelay);
+
+  selectedText = controller.tabs.activeTabWindow.getSelection();
+  if (selectedText.toString().toLowerCase() != searchedText) {
+    throw "Searched string does not match Selected string!"
+  }
+}
