fa40d17dbe6c1299cef7aa27b098a55f527ee8e7
Henrik Skupin <hskupin@mozilla.com>
2009-07-16 01:23:44 +0200

    Bug 493226: Click in search bar has to take care of overlaying engine button

--- firefox/testSearch/testFocusAndSearch.js
+++ firefox/testSearch/testFocusAndSearch.js
@@ -78,9 +78,9 @@ var doSearch = function(searchTerm) {
 }
 
 var testSearchBarFocusAndSearch = function() {
-  // Click search field and start search
-  var searchTextbox = new elementslib.Lookup(controller.window.document, '/id("main-window")/id("navigator-toolbox")/id("nav-bar")/id("search-container")/id("searchbar")/anon({"anonid":"searchbar-textbox"})/anon({"class":"autocomplete-textbox-container"})/anon({"anonid":"textbox-input-box"})');
-  controller.click(searchTextbox);
+  // The engine button overlays the textbox so click 1px behind the button
+  var engineButton = new elementslib.Lookup(controller.window.document, '/id("main-window")/id("navigator-toolbox")/id("nav-bar")/id("search-container")/id("searchbar")/anon({"anonid":"searchbar-textbox"})/anon({"anonid":"searchbar-engine-button"})');
+  controller.click(searchBar, engineButton.getNode().clientWidth, 1);
   doSearch("Firefox");
 
   // Use shortcut to start search
