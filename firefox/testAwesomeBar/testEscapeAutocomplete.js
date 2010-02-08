8a16680d85278610d7797ddcb426113904524fae
Tracy Walker <twalker@mozilla.com>
2010-02-08 18:57:48 +0100

    Bug 544014 - Escape should dismiss autocomplete then undo entered text in location bar, r=hskupin

--- firefox/testAwesomeBar/testEscapeAutocomplete.js
+++ firefox/testAwesomeBar/testEscapeAutocomplete.js
@@ -14,12 +14,11 @@
  * The Original Code is MozMill Test code.
  *
  * The Initial Developer of the Original Code is Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2009
+ * Portions created by the Initial Developer are Copyright (C) 2010
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
  *   Tracy Walker <twalker@mozilla.com>
- *   Henrik Skupin <hskupin@mozilla.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -33,24 +32,20 @@
  * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
- * **** END LICENSE BLOCK *****/
+ * ***** END LICENSE BLOCK *****/
 
 // Include necessary modules
 var RELATIVE_ROOT = '../../shared-modules';
 var MODULE_REQUIRES = ['PlacesAPI', 'ToolbarAPI'];
 
-const gTimeout = 5000;
+const gTimeout = 1000;
 const gDelay = 100;
 
-const websites = ['http://www.google.com/',
-                  'http://www.mozilla.org/',
-                  'http://www.getpersonas.com/',
-                  'about:blank'];
-
-var setupModule = function(module) {
-  controller = mozmill.getBrowserController();
-  locationBar = new ToolbarAPI.locationBar(controller);
-
+var setupModule = function(module) 
+{
+  module.controller = mozmill.getBrowserController();
+  module.locationBar =  new ToolbarAPI.locationBar(controller);
+  
   // Clear complete history so we don't get interference from previous entries
   try {
     PlacesAPI.historyService.removeAllPages();
@@ -58,47 +53,47 @@ var setupModule = function(module) {
 }
 
 /**
- * Check acces to the location bar drop down list via autocomplete
+ * Check Escape key functionality during auto-complete process
  */
-var testAccessLocationBarHistory = function()
+var testEscape = function()
 {
-  // Open a few different sites to create a small history (about:blank doesn't
-  // appear in history and clears the page for clean test arena
+  var websites = ['http://www.google.com/', 'http://www.mozilla.org'];
+
+  // Open some pages to set up the test environment
   for each (website in websites) {
     locationBar.loadURL(website);
     controller.waitForPageLoad();
   }
 
-  // Wait about 4s so the history gets populated
+  // wait for 4 seconds to work around Firefox LAZY ADD of items to the db
   controller.sleep(4000);
 
-  // First - Focus the locationbar then delete any contents there
+  var testString = "google";
+  
+  // Focus the locationbar and delete any contents there
   locationBar.clear();
 
-  // Second - Arrow down to open the autocomplete list (displays most recent visit first),
-  // then arrow down again to the first entry, in this case www.getpersonas.com;
-  controller.keypress(locationBar.urlbar, "VK_DOWN", {});
-  controller.sleep(gDelay);
-  controller.keypress(locationBar.urlbar, "VK_DOWN", {});
-  controller.sleep(gDelay);
+  // Use type and sleep on each letter to allow the autocomplete to populate with results. 
+  for (var i = 0; i < testString.length; i++) {
+    locationBar.type(testString[i]);
+    controller.sleep(gDelay);
+  }
 
-  // checks that the first item in the drop down list is selected.
-  controller.waitForEval("subject.selectedIndex == 0",
-                         gTimeout, 100, locationBar.autoCompleteResults);
-  locationBar.contains("getpersonas");
-  controller.keypress(null, "VK_RETURN", {});
-  controller.waitForPageLoad();
+  // confirm that google is in the locationbar and the awesomecomplete list is open
+  controller.assertJS("subject.contains('" + testString + "') == true", locationBar);
+  controller.assertJS("subject.autoCompleteResults.isOpened == true", locationBar);
 
-  // Finally - Check that the personas page was loaded
-  // Check for presense of Personsas image
-  var personasImage = new elementslib.XPath(controller.tabs.activeTab, "/html/body/div[@id='outer-wrapper']/div[@id='inner-wrapper']/div[@id='nav']/h1/a/img");
-  controller.waitForElement(personasImage, gTimeout, 100);
-
-  // Check that getpersonas is in the url bar
-  locationBar.contains("getpersonas");
+  // After first Escape, confirm that google is in the locationbar and awesomecomplete list is closed
+  controller.keypress(locationBar.urlbar, 'VK_ESCAPE', {});
+  controller.assertJS("subject.contains('" + testString + "') == true", locationBar);
+  controller.assertJS("subject.autoCompleteResults.isOpened == false", locationBar);
+  
+  // After second Escape, confirm the locationbar returns to the current page url
+  controller.keypress(locationBar.urlbar, 'VK_ESCAPE', {});
+  controller.assertJS("subject.contains('" + websites[1] + "') == true", locationBar);
 }
 
 /**
- * Map test functions to litmus tests
+ * Map test function to litmus test
  */
-testAccessLocationBarHistory.meta = {litmusids : [5981]};
+testEscape.meta = {litmusids : [8693]};
