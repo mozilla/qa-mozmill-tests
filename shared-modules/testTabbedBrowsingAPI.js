e108a9f355fdaf50e26e00204fc954b67e40da64
Henrik Skupin <hskupin@mozilla.com>
2010-03-17 10:40:02 +0100

    Bug 552594 - Interim patch to fix clicking a tab on Linux due to rounded corners, r=ashughes
    --HG--
    branch : mozilla1.9.1

--- shared-modules/testTabbedBrowsingAPI.js
+++ shared-modules/testTabbedBrowsingAPI.js
@@ -118,7 +118,7 @@ tabBrowser.prototype = {
    *        Index of the tab which should be selected
    */
   set selectedIndex(index) {
-    this._controller.click(this.getTab(index));
+    this._controller.click(this.getTab(index), 2, 2);
   },
 
   /**
