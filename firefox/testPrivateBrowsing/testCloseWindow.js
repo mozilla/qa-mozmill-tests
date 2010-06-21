56de2f63baafdcd3a7dc5369663dfe8cf9647348
Aaron Train <atrain@mozilla.com>
2010-06-21 15:36:27 -0400

    Bug 573461 - Timeout failure in testDisabledElements.js. r=ashughes, sr=hskupin
    --HG--
    branch : mozilla1.9.2

--- firefox/testPrivateBrowsing/testCloseWindow.js
+++ firefox/testPrivateBrowsing/testCloseWindow.js
@@ -19,6 +19,7 @@
  *
  * Contributor(s):
  *   Henrik Skupin <hskupin@mozilla.com>
+ *   Aaron Train <atrain@mozilla.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -103,7 +104,7 @@ var testCloseWindow = function() {
                          gTimeout, 100,
                          {utils: mozmill.utils, expectedCount: windowCount});
 
-  UtilsAPI.handleWindow("type", "navigator:browser", checkWindowOpen);
+  UtilsAPI.handleWindow("type", "navigator:browser", checkWindowOpen, true);
 }
 
 function checkWindowOpen(controller) {
