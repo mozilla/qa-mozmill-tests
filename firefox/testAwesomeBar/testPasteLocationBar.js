66c5fe345937f3e401bfe701c06610bd4303f137
Al Billings <abillings@mozilla.com>
2010-06-23 15:40:18 +0200

    Bug 553616 - Context menu: Paste string into location bar, r=hskupin
    --HG--
    branch : mozilla1.9.1

---
+++ firefox/testAwesomeBar/testPasteLocationBar.js
@@ -0,0 +1,54 @@
+/** Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+// Include necessary modules
+const RELATIVE_ROOT = '../../shared-modules';
+const MODULE_REQUIRES = ['PlacesAPI', 'ToolbarAPI', 'UtilsAPI'];
+
+const TIMEOUT = 5000;
+
+const LOCAL_TEST_FOLDER = collector.addHttpResource('../test-files/');
+
+var setupModule = function(module) {
+  controller = mozmill.getBrowserController();
+  locationBar =  new ToolbarAPI.locationBar(controller);
+
+  // Clear complete history so we don't get interference from previous entries
+  PlacesAPI.removeAllHistory();
+}
+
+var teardownModule = function() {
+  locationBar.closeContextMenu();
+}
+
+/**
+ * Grab some text from a web page and then paste it into the toolbar
+ *
+ */
+var testPasteLocationBar = function() {
+  // Open the test page
+  controller.open(LOCAL_TEST_FOLDER + "awesomebar/copypaste.html");
+  controller.waitForPageLoad();
+
+  // Focus on page, select text and copy to clipboard
+  ipsumLocation = new elementslib.ID(controller.window.document, 'ipsum');
+  controller.doubleClick(ipsumLocation);
+  var docSelection = controller.tabs.activeTabWindow.getSelection().toString();
+
+  // Copy "ipsum" into clipboard  
+  controller.keypress(null, "c", {accelKey: true});
+
+  // Clear the locationBar
+  locationBar.clear();
+
+  // Get the urlbar input box, right click in it, and select paste from context menu
+  var input = locationBar.getElement({type: "urlbar_input"});
+  controller.rightClick(input);
+  var contextMenuEntry = locationBar.getElement({type: "contextMenu_entry", subtype: "paste"});
+  controller.click(contextMenuEntry);
+
+  // Get contents of locationbar and compare it to expected result  
+  controller.waitForEval("subject.urlbar.value == subject.selectedText", TIMEOUT, 100,
+                         {urlbar: locationBar.urlbar.getNode(), selectedText: docSelection});
+}
