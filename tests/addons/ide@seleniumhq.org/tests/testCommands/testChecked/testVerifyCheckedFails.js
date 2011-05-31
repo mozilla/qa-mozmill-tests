e7186e34d06ee35307799ce22ba4cecbd5dc829a
Dave Hunt <dhunt@mozilla.com>
2011-06-01 00:13:58 +0100

    Bug 651242 - Add tests for *Checked commands in Selenium IDE. r=hskupin
    --HG--
    branch : mozilla-aurora

---
+++ tests/addons/ide@seleniumhq.org/tests/testCommands/testChecked/testVerifyCheckedFails.js
@@ -0,0 +1,68 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozmill Test Code.
+ *
+ * The Initial Developer of the Original Code is the Mozilla Foundation.
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Dave Hunt <dhunt@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+// Include required modules
+var selenium = require("../../../lib/selenium");
+var checks = require("../../../lib/checks");
+
+function setupModule(module) {
+  controller = mozmill.getBrowserController();
+  sm = new selenium.SeleniumManager();
+  sm.open(controller);
+}
+
+function teardownModule(module) {
+  sm.close();
+}
+
+function testVerifyCheckedCommandFails() {
+  sm.baseURL = "chrome://selenium-ide/";
+  sm.addCommand({action: "open",
+                target: "/content/tests/functional/aut/checksandradios.html"});
+  sm.addCommand({action: "verifyChecked",
+                target: "css=input[name=elephants]"});
+  sm.addCommand({action: "echo",
+                target: "final command"});
+  sm.playTest();
+
+  checks.commandFailed(sm, "false");
+  
+  //check final command is executed
+  sm.controller.assert(function () {
+    return sm.finalLogInfoMessage === "echo: final command";
+  }, "Final command was executed, got '" + sm.finalLogInfoMessage +"' expected 'echo: final command'");
+}
