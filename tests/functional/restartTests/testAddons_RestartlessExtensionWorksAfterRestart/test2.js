a1104008a66b67203ad3392736c6a225c93f4c16
Remus Pop <remus.pop@softvision.ro>
2011-09-29 16:44:59 +0300

    Bug 680918 - Mozmill test for checking if restartless extension works after restart r=alex.lakatos, r=ashughes
    --HG--
    branch : mozilla-release

---
+++ tests/functional/restartTests/testAddons_RestartlessExtensionWorksAfterRestart/test2.js
@@ -0,0 +1,84 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is MozMill Test code.
+ *
+ * The Initial Developer of the Original Code is the Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Vlad Maniac <vlad.maniac@softvisioninc.eu> (original author)
+ *   Remus Pop <remus.pop@softvision.ro>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+// Include required modules
+var {assert} = require("../../../../lib/assertions");
+var prefs = require("../../../../lib/prefs");
+var tabs = require("../../../../lib/tabs");
+var utils = require("../../../../lib/utils");
+
+var EXPECTED_URL = "http://mozqa.com/data/firefox/layout/mozilla.html";
+
+function setupModule() {
+  controller = mozmill.getBrowserController();
+  tabs.closeAllTabs(controller);
+}
+
+function teardownModule() {
+  prefs.preferences.clearUserPref("browser.urlbar.trimURLs");
+
+  delete persisted.addon;
+}
+
+/**
+ * Test that verifies the addon works after browser restart
+ */
+function testRestartlessExtensionWorksAfterRestart() {
+  // Change pref to show the full url in the location bar
+  prefs.preferences.setPref("browser.urlbar.trimURLs", false);
+
+  // Context menu item that is provided by the restartless extension
+  var contextMenuItem = new elementslib.ID(controller.window.document,
+                                           persisted.addon.id +
+                                           "-context-menu-item-0");
+
+  var locationBar = new elementslib.ID(controller.window.document, "urlbar");
+
+  // Open content area context menu
+  controller.rightClick(new elementslib.XPath(controller.tabs.activeTab, "/html"));
+
+  // Click the item from the context menu to open mozilla.html from mozqa.com
+  controller.click(contextMenuItem);
+
+  // Close the context menu
+  utils.closeContentAreaContextMenu(controller);
+  controller.waitForPageLoad();
+
+  // Verify that the loaded url matches http://mozqa.com/data/firefox/layout/mozilla.html
+  assert.equal(locationBar.getNode().value, EXPECTED_URL,
+               "Current URL should match expected URL");
+}
