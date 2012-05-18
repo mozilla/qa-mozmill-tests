c2cdfced385907ef036d8e545374157fb833ee54
Remus Pop <remus.pop@softvision.ro>
2012-05-18 12:53:15 +0300

    Bug 701901 - enable testGetMoreSearchEngines r=dave.hunt
    --HG--
    branch : mozilla-release
    extra : transplant_source : %01%DB%FF%B7%40%B4%97%3Bz%85%B6%F2%E5%40%29%E8yy%CC%EF

--- tests/functional/testSearch/testGetMoreSearchEngines.js
+++ tests/functional/testSearch/testGetMoreSearchEngines.js
@@ -121,10 +121,3 @@ var handleSearchInstall = function(controller)
                                          '/id("commonDialog")/anon({"anonid":"buttons"})/{"dlgtype":"accept"}')
   controller.waitThenClick(addButton);
 }
-
-// XXX: "Bug 701901 - Failure in testGetMoreSearchEngines.js: Modal dialog has been found and processed"
-setupModule.__force_skip__ = "Bug 701901 - Failure in testGetMoreSearchEngines.js: " + 
-                             "Modal dialog has been found and processed";
-teardownModule.__force_skip__ = "Bug 701901 - Failure in testGetMoreSearchEngines.js: " + 
-                                "Modal dialog has been found and processed";
-
