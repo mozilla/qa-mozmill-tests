1c88ed17fee719e0cf57154f856dde799cda83a6
Henrik Skupin <hskupin@mozilla.com>
2012-06-19 09:09:23 +0200

    Backout skipped user restart tests due to fix in Mozmill 1.5.13
    --HG--
    branch : mozilla-release
    extra : transplant_source : %B7%CC%C5%08C%BBTnO%DB.V%BF%99%05%97zA5%D5

--- tests/functional/restartTests/testAddons_changeTheme/test2.js
+++ tests/functional/restartTests/testAddons_changeTheme/test2.js
@@ -46,8 +46,6 @@ function testThemeIsInstalled() {
   var restartLink = addonsManager.getElement({type: "listView_restartLink", 
                                               parent: defaultTheme});
 
-  // XXX Bug 747418
-  // startUserShutdown is broken. Needs to wait for Mozmill 2.0
-  //controller.startUserShutdown(TIMEOUT_USER_SHUTDOWN, true);
-  //controller.click(restartLink); 
+  controller.startUserShutdown(TIMEOUT_USER_SHUTDOWN, true);
+  controller.click(restartLink); 
 }
