cd966be969d97bda3b996e16248f6de5d5e855b6
Henrik Skupin <hskupin@mozilla.com>
2012-07-22 21:54:29 +0200

    Bug 761984 - Relanded skip patch for testFocusAndSearch.js

--- tests/functional/testSearch/testFocusAndSearch.js
+++ tests/functional/testSearch/testFocusAndSearch.js
@@ -42,3 +42,6 @@ var testShortcutAndSearch = function()
  */
 // testClickAndSearch.meta = {litmusids : [8241]};
 // testShortcutAndSearch.meta = {litmusids : [8242]};
+
+setupModule.__force_skip__ = "Bug 761984 - Failure in testFocusAndSearch |" +
+                             "Disconnect Error: Application unexpectedly closed";
