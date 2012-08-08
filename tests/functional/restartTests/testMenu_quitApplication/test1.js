88e97c770bebfbf4d5a14d4d888efd8ccc3319e0
Alex Lakatos <alex.lakatos@softvision.ro>
2012-08-08 18:42:21 +0300

    Bug 757916 - Mozmill test for quitting the application. r=dhunt r=hskupin

---
+++ tests/functional/restartTests/testMenu_quitApplication/test1.js
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+function setupModule() {
+  controller = mozmill.getBrowserController();
+}
+
+/*
+ * Test quitting the application
+ */
+function testQuitApplication() {
+  controller.startUserShutdown(4000, false);
+  controller.mainMenu.click("#menu_FileQuitItem");
+}
+
+/**
+ * Map test functions to moztrap tests
+ */
+testQuitApplication.meta = {moztrap_case: 333};
