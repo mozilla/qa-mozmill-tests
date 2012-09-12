ccd37d2204f4d487121340d7548cad5d098e2520
Henrik Skupin <hskupin@mozilla.com>
2012-09-12 19:22:32 +0200

    Backed out changeset 56c01340f467

--- tests/functional/testAwesomeBar/testCheckItemHighlight.js
+++ tests/functional/testAwesomeBar/testCheckItemHighlight.js
@@ -87,16 +87,16 @@ function checkAwesomebarResults(aResult, aType) {
                    getUnderlinedText(aResult, aType);
 
   // Check that there is only 1 entry
-  controller.waitFor(function () {
+  controller.assert(function () {
     return underlined.length === 1;
-  }, "Only one autocompleted result is underlined");
+  }, "Only one autocompleted result is underlined - got '" +
+     underlined.length + "', expected '1'");
 
   // Check that the underlined URL matches the entered URL
   underlined.forEach(function (element) {
-    controller.waitFor(function() {
+    controller.assert(function() {
       return element.toLowerCase() === LOCAL_TEST_PAGES[0].name;
-    }, "Underlined " + aType + " matches entered " + aType +
-       " - expected '" + LOCAL_TEST_PAGES[0].name + "'");
+    }, "Underlined " + aType + " matches entered " + aType + " - got '" +
+       element.toLowerCase() + "', expected '" + LOCAL_TEST_PAGES[0].name + "'");
   });
 }
-
