28590977d0f67d9989616f90a72578b3923caafa
Henrik Skupin <hskupin@mozilla.com>
2012-10-25 10:43:04 -0400

    Bug 784305 - Renable /restartTests/testAddons_RestartlessExtensionWorksAfterRestart

--- tests/functional/restartTests/testAddons_RestartlessExtensionWorksAfterRestart/test2.js
+++ tests/functional/restartTests/testAddons_RestartlessExtensionWorksAfterRestart/test2.js
@@ -57,6 +57,3 @@ function testRestartlessExtensionWorksAfterRestart() {
                "Current URL should match expected URL");
 }
 
-setupModule.__force_skip__ = "Bug 784305 - Current URL should match expected URL";
-teardownModule.__force_skip__ = "Bug 784305 - Current URL should match expected URL";
-
