7bde61007fac4db3b62d03a8ac4e363b2e7f8416
Henrik Skupin <hskupin@mozilla.com>
2012-10-27 14:00:09 -0400

    Bug 806097 - Update autocomplete=off test to use a local page instead of google.com. r=dhunt

--- tests/functional/testFormManager/testAutoCompleteOff.js
+++ tests/functional/testFormManager/testAutoCompleteOff.js
@@ -2,40 +2,49 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const gDelay = 0;
-const gTimeout = 5000;
+var { expect } = require("../../../lib/assertions");
+
+const LOCAL_TEST_FOLDER = collector.addHttpResource('../../../data/');
+const LOCAL_TEST_PAGE = LOCAL_TEST_FOLDER + 'form_manager/autocomplete.html';
+
+const SEARCH_TERM = "mozillazine";
 
 var setupModule = function(module) {
   module.controller = mozmill.getBrowserController();
 }
 
+/**
+ * Tests that no auto-complete entries are shown if turned off
+ */
 var testAutoCompleteOff = function() {
-  var url = "http://www.google.com/webhp?complete=1&hl=en";
-  var searchTerm = "mozillazine";
-
-  // Open the google homepage
-  controller.open(url);
+  controller.open(LOCAL_TEST_PAGE);
   controller.waitForPageLoad();
 
-  // Search for mozillazine on google
-  var searchField = new elementslib.Name(controller.tabs.activeTab, "q");
-  var submitButton = new elementslib.Name(controller.tabs.activeTab, "btnG");
+  var searchField = new elementslib.ID(controller.tabs.activeTab, "search");
+  var submitButton = new elementslib.ID(controller.tabs.activeTab, "submit");
 
   controller.waitForElement(searchField);
-  controller.type(searchField, searchTerm);
+  controller.type(searchField, SEARCH_TERM);
+
   controller.click(submitButton);
+  controller.waitForPageLoad();
+
+  var searchTerm = new elementslib.ID(controller.tabs.activeTab, "term");
+  expect.equal(searchTerm.getNode().innerHTML, SEARCH_TERM,
+               "Search for the given search term happened.");
 
   // Go back to the search page
-  controller.open(url);
+  controller.open(LOCAL_TEST_PAGE);
   controller.waitForPageLoad();
 
   // Enter a part of the search term only
-  controller.waitForElement(searchField, gTimeout);
-  controller.type(searchField, searchTerm.substring(0, 3));
+  controller.waitForElement(searchField);
+  controller.type(searchField, SEARCH_TERM.substring(0, 3));
   controller.sleep(500);
 
   // Verify source autocomplete=off
-  var popupAutoCompList = new elementslib.ID(controller.window.document, "PopupAutoComplete");
+  var popupAutoCompList = new elementslib.ID(controller.window.document,
+                                             "PopupAutoComplete");
   controller.assertJSProperty(popupAutoCompList, "popupOpen", false);
 }
 
