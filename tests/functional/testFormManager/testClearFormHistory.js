ef3de4839b92573ddfa18f08c13a73e2177e7a9c
Mario Garbi <mario.garbi@softvision.ro>
2012-12-17 16:49:31 +0200

    Bug 809434 - Change assert to expect for former controller.assertChecked() calls. r=dhunt, r=hskupin, r=amatei
    --HG--
    branch : mozilla-release
    extra : transplant_source : %12%5D%82%855%A53%129v%28%23%DDa4%CD%DAV%1A%B5

--- tests/functional/testFormManager/testClearFormHistory.js
+++ tests/functional/testFormManager/testClearFormHistory.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Include required modules
+var { assert } = require("../../../lib/assertions");
 var modalDialog = require("../../../lib/modal-dialog");
 
 const TIMEOUT = 5000;
@@ -77,7 +78,7 @@ function clearHistoryHandler(controller) {
                                        "/*[name()='listbox'][1]" +
                                        "/*[name()='listitem'][2]");
   controller.waitForElement(checkBox, TIMEOUT);
-  controller.assertChecked(checkBox);
+  assert.ok(checkBox.getNode().checked, "The checkbox to clear form data is checked");
 
   var clearButton = new elementslib.Lookup(controller.window.document,
                                            '/id("SanitizeDialog")' +
