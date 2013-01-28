21aadfbc179c6b94b6cbf930ef02aa56d7e0e169
Henrik Skupin <hskupin@mozilla.com>
2013-01-28 12:21:26 +0100

    Backout for Bug 813170 - Disable testAddons_RestartlessExtensionWorksAfterRestart due to failure 'Disconnect Error: Application unexpectedly closed'
    --HG--
    branch : mozilla-esr10

--- tests/functional/restartTests/testAddons_RestartlessExtensionWorksAfterRestart/test2.js
+++ tests/functional/restartTests/testAddons_RestartlessExtensionWorksAfterRestart/test2.js
@@ -57,9 +57,3 @@ function testRestartlessExtensionWorksAfterRestart() {
   assert.equal(locationBar.getNode().value, EXPECTED_URL,
                "Current URL should match expected URL");
 }
-
-setupModule.__force_skip__ = "Bug 813170 - Test failure 'Disconnect Error: " +
-                             "Application unexpectedly closed'";
-teardownModule.__force_skip__ = "Bug 813170 - Test failure 'Disconnect Error: " +
-                                "Application unexpectedly closed'";
-
