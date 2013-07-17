43c74ab91c652099cc0e9c714bec22b15760ad48
Andrei Eftimie <andrei.eftimie@softvision.ro>
2013-07-17 12:21:58 +0300

    Bug 894509 - Switched new tab page title check to newtab.pageTitle. r=amatei
    --HG--
    branch : mozilla-beta
    extra : transplant_source : %AF%A2%7F%FC%8D%8Au%8E%00o%FC%CB%2B%BDH%C0X%19%C9%5C

--- tests/functional/testTabbedBrowsing/testNewTab.js
+++ tests/functional/testTabbedBrowsing/testNewTab.js
@@ -79,8 +79,7 @@ function checkOpenTab(aEventType) {
                "Opened new tab");
 
   // The tabs title should be 'New Tab'
-  var title = utils.getProperty("chrome://browser/locale/tabbrowser.properties",
-                                "tabs.emptyTabTitle");
+  var title = utils.getEntity(tabBrowser.getDtds(), "newtab.pageTitle");
 
   expect.equal(tabBrowser.getTab().getNode().label, title, "Correct tab title");
 
