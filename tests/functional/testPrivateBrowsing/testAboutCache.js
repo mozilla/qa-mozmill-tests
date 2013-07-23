406a19746a9f2728c0bd20d1b0fc0d546c19e423
Andreea Matei <andreea.matei@softvision.ro>
2013-07-23 10:55:29 +0300

    Bug 873974 - Make sure to clear cache after closing all tabs in /testPrivateBrowsing/testAboutCache.js. r=hskupin, r=dhunt
    --HG--
    branch : mozilla-release
    extra : transplant_source : %FEG%E99%F7%1C%1D%E5P%E1%90Hw%BE%EB%A8%3D%E17%83

--- tests/functional/testPrivateBrowsing/testAboutCache.js
+++ tests/functional/testPrivateBrowsing/testAboutCache.js
@@ -16,10 +16,11 @@ function setupModule() {
   controller = mozmill.getBrowserController();
   pbWindow = new privateBrowsing.PrivateBrowsingWindow();
 
-  // Clear cache
-  Services.cache.evictEntries(Ci.nsICache.STORE_ANYWHERE);
-
   tabs.closeAllTabs(controller);
+
+  // Clear cache after closing all tabs in order to avoid
+  // having about:newtab as a disk entry
+  Services.cache.evictEntries(Ci.nsICache.STORE_ANYWHERE);
 }
 
 function teardownModule() {
