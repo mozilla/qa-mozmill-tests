884b878a432ca38867515ce7e9eb7f1c4bc5b0ed
Andreea Matei <andreea.matei@softvision.ro>
2013-09-11 16:14:35 +0300

    Bug 915139 - Skip testMixedContentPage.js due to failures. r=aeftimie

--- tests/functional/testSecurity/testMixedContentPage.js
+++ tests/functional/testSecurity/testMixedContentPage.js
@@ -39,3 +39,5 @@ function testMixedContentPage() {
                                    "identity.mixed_display_loaded_active_blocked");
   assert.equal(encryptionPopup.getNode().textContent, property, "The page has mixed content");
 }
+
+setupModule.__force_skip__ = "Bug 915139 - Test failure 'The page has mixed content'"
