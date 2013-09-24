7fb49a92eb3f549cef417066863640fd3ebf28b7
Mario Garbi <mario.garbi@softvision.ro>
2013-09-24 12:45:37 +0300

    Bug 919932 - Clean up the duplicated teardownModules. r=amatei r=aeftimie
    --HG--
    branch : mozilla-beta

--- tests/functional/restartTests/testAddons_uninstallExtension/test1.js
+++ tests/functional/restartTests/testAddons_uninstallExtension/test1.js
@@ -51,15 +51,6 @@ function teardownModule(aModule) {
   }
 }
 
-function teardownModule() {
-  // Bug 867217
-  // Mozmill 1.5 does not have the restartApplication method on the controller.
-  // Remove condition when transitioned to 2.0
-  if ("restartApplication" in controller) {
-    controller.restartApplication();
-  }
-}
-
 /*
  * Install some add-ons to test uninstallation
  */
