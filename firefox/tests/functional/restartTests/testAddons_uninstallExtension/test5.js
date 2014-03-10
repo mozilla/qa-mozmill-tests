d6504b5fdc963d8f3b8a68e184ec23d286d97955
Andrei Eftimie <andrei.eftimie@softvision.ro>
2014-03-10 14:30:48 +0200

    Bug 783484 - Re-enable tests from restartTests/testAddons_uninstallExtension/. r=amatei

--- firefox/tests/functional/restartTests/testAddons_uninstallExtension/test5.js
+++ firefox/tests/functional/restartTests/testAddons_uninstallExtension/test5.js
@@ -50,8 +50,3 @@ function testEnabledExtensionIsUninstalled() {
                "Extension '" + persisted.addons[0].id +
                "' has been uninstalled");
 }
-
-setupModule.__force_skip__ = "Bug 783484 -  Test failure 'Shutdown expected " +
-                             "but none detected before end of test";
-teardownModule.__force_skip__ = "Bug 783484 -  Test failure 'Shutdown expected " +
-                                "but none detected before end of test";
