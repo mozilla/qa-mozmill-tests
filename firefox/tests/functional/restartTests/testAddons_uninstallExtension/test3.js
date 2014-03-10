a8685e12947ff20d87219a2350c9b11498eb3d0e
Andrei Eftimie <andrei.eftimie@softvision.ro>
2014-03-10 14:30:48 +0200

    Bug 783484 - Re-enable tests from restartTests/testAddons_uninstallExtension/. r=amatei
    --HG--
    branch : mozilla-esr24

--- firefox/tests/functional/restartTests/testAddons_uninstallExtension/test3.js
+++ firefox/tests/functional/restartTests/testAddons_uninstallExtension/test3.js
@@ -20,12 +20,10 @@ function setupModule(aModule) {
 
 function teardownModule(aModule) {
   // Bug 886811
-  // Mozmill 1.5 does not have the stopApplication method on the controller.
+  // Mozmill 1.5 does not have the restartApplication method on the controller.
   // Remove condition when transitioned to 2.0
-  if ("stopApplication" in aModule.controller) {
-    // Bug 783484
-    // Revert to restartApplication once test3 and test4 are reenabled
-    aModule.controller.stopApplication(true);
+  if ("restartApplication" in aModule.controller) {
+    aModule.controller.restartApplication();
   }
 }
 
