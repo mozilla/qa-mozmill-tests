edc7150507e92946f112675df2f3c3ddbb82e932
Andrei Eftimie <andrei.eftimie@softvision.ro>
2014-07-01 16:42:22 +0300

    Bug 1015126 - Disable due to waitForPageLoad failures. r=amatei

--- firefox/tests/functional/testPreferences/testSetToCurrentPage.js
+++ firefox/tests/functional/testPreferences/testSetToCurrentPage.js
@@ -74,3 +74,8 @@ function prefDialogHomePageCallback(controller) {
 
   prefDialog.close(true);
 }
+
+if (mozmill.isLinux) {
+  setupModule.__force_skip__ = "Bug 1015126 - waitForPageLoad failure";
+  teardownModule.__force_skip__ = "Bug 1015126 - waitForPageLoad failure";
+}
