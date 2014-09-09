296bd3f7d215bba266db06583daad5b0d7b12e24
Andrei Eftimie <andrei.eftimie@softvision.ro>
2014-09-09 15:18:23 +0300

    Backed out changeset aeb8e247acf5

---
+++ firefox/tests/functional/restartTests/testAddons_installUninstallSoftBlocklistedExtension/test4.js
@@ -0,0 +1,48 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// Include required modules
+var addons = require("../../../../../lib/addons");
+var { assert } = require("../../../../../lib/assertions");
+var prefs = require("../../../../lib/prefs");
+var utils = require("../../../../../lib/utils");
+
+const PREF_BLOCKLIST = "extensions.blocklist.url";
+const PREF_INSTALL_DIALOG = "security.dialog_enable_delay";
+const PREF_LAST_CATEGORY = "extensions.ui.lastCategory";
+
+function setupModule(aModule) {
+  aModule.controller = mozmill.getBrowserController();
+  aModule.addonsManager = new addons.AddonsManager(aModule.controller);
+}
+
+function teardownModule(aModule) {
+  prefs.preferences.clearUserPref(PREF_BLOCKLIST);
+  prefs.preferences.clearUserPref(PREF_INSTALL_DIALOG);
+  prefs.preferences.clearUserPref(PREF_LAST_CATEGORY);
+
+  // Reset the blocklist
+  utils.updateBlocklist();
+
+  delete persisted.addon;
+
+  addons.resetDiscoveryPaneURL();
+  aModule.addonsManager.close();
+
+  aModule.controller.stopApplication(true);
+}
+
+/**
+ * Test the blocklisted extension has been uninstalled
+ */
+function testBlocklistedExtensionUninstalled() {
+  addonsManager.open();
+
+  var addons = addonsManager.getAddons({attribute: "value",
+                                        value: persisted.addon.id});
+
+  assert.equal(addons.length, 0, "The blocklisted add-on is uninstalled");
+}
