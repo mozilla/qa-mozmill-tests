e59a9a169f598212c21219c69fbd9d64fd685288
Daniel Gherasim <daniel.gherasim@softvision.ro>
2014-10-14 10:59:28 +0300

    Bug 1080569 - Multiple test failures in testSearch/ related to search engines. r=aeftimie, r=amatei, r=hskupin
    --HG--
    branch : mozilla-beta
    extra : transplant_source : %C1q9%F1%BDYb%EA%0F%9A%FF%A21%5Df%E4%85W%E0B

--- firefox/lib/search.js
+++ firefox/lib/search.js
@@ -515,7 +515,7 @@ searchBar.prototype = {
 
     this.enginesDropDownOpen = state;
 
-    return engine.getNode().id;
+    return engine.getNode().label;
   },
 
   /**
@@ -529,7 +529,7 @@ searchBar.prototype = {
       // Open drop down and click on search engine
       this.enginesDropDownOpen = true;
 
-      var engine = this.getElement({type: "engine", subtype: "id", value: aName});
+      var engine = this.getElement({type: "engine", subtype: "label", value: aName});
       var engineChanged = false;
       var observer = {
         observe: (aSubject, aTopic, aData) => {
