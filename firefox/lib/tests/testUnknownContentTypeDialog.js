e50e5bbd72e123f96877e261164ad138ee08eb76
Daniel Gherasim <daniel.gherasim@softvision.ro>
2014-10-22 15:46:32 +0300

    Bug 1081047 - Add new ui module for the unknownContentType dialog. r=aeftimie, r=amatei, r=hskupin
    --HG--
    branch : mozilla-aurora
    extra : transplant_source : %5C%F3%A5%B9l%20%E6l%22%B4%29Z%E6f%92%40%AA%C4sf

---
+++ firefox/lib/tests/testUnknownContentTypeDialog.js
@@ -0,0 +1,51 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// Include required modules
+var browser = require("../ui/browser");
+var windows = require("../../../lib/windows");
+
+const TEST_DATA = {
+  url: "ftp://ftp.mozilla.org/pub/firefox/releases/3.6/mac/en-US/Firefox%203.6.dmg",
+  elements: {
+    cancel: "button",
+    accept: "button",
+    remember: "checkbox",
+    save: "radio"
+  }
+};
+
+function setupModule(aModule) {
+  aModule.browserWindow = new browser.BrowserWindow();
+}
+
+function teardownModule(aModule) {
+  windows.closeAllWindows(aModule.browserWindow);
+}
+
+/**
+ * Bug 1081047
+ * Add new ui module for the unknownContentType dialog
+ */
+function testUnknownContentTypeDialog() {
+  var dialog = browserWindow.openUnknownContentTypeDialog(() => {
+    browserWindow.controller.open(TEST_DATA.url);
+  });
+
+  for (var element in TEST_DATA.elements) {
+    if (TEST_DATA.elements[element] === "button") {
+      var el = dialog.getElement({type: TEST_DATA.elements[element],
+                                  subtype: element});
+    }
+    else {
+      var el = dialog.getElement({type: element});
+    }
+    expect.equal(el.getNode().localName, TEST_DATA.elements[element],
+                 "Elemens has been found - " + element);
+  }
+
+  dialog.close();
+}
---
+++ firefox/lib/ui/unknown-content-type-dialog.js
@@ -0,0 +1,99 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// Include required modules
+var baseWindow = require("../../../lib/ui/base-window");
+var domUtils = require("../../../lib/dom-utils");
+var windows = require("../../../lib/windows");
+
+/**
+ * Save file of unknown type window class
+ *
+ * @constructor
+ * @param {MozMillController} [aController]
+ *        MozMillController of the SaveFile Window
+ */
+function UnknownContentTypeDialog(aController) {
+  baseWindow.BaseWindow.call(this, aController);
+
+  this._dtds = ["chrome://branding/locale/brand.dtd",
+                "chrome://mozapps/locale/downloads/unknownContentType.dtd",
+                "chrome://mozapps/locale/downloads/settingsChange.dtd"];
+
+  this._properties = ["chrome://mozapps/locale/downloads/unknownContentType.properties"];
+}
+
+UnknownContentTypeDialog.prototype = Object.create(baseWindow.BaseWindow.prototype);
+UnknownContentTypeDialog.prototype.constructor = UnknownContentTypeDialog;
+
+/**
+ * Retrieve list of UI elements based on the given specification
+ *
+ * @param {object} aSpec
+ *        Information of the UI elements which should be retrieved
+ * @parma {string} aSpec.type
+ *        Identifier of the elements
+ * @param {string} [aSpec.subtype]
+ *        Attribute of the elements to filter
+ * @param {string} [aSpec.value]
+ *        Value of the attribute to filter
+ * @param {string} [aSpec.parent=document]
+ *        Parent of the to find element
+ *
+ * @returns {ElemBase[]} Elements which have been found
+ */
+UnknownContentTypeDialog.prototype.getElements = function UCTD_getElements(aSpec={}) {
+  var elems = [];
+  var root = aSpec.parent ? aSpec.parent.getNode()
+                          : this._controller.window.document;
+  var nodeCollector = new domUtils.nodeCollector(root.documentElement);
+
+  switch (aSpec.type) {
+    case "button":
+      assert.ok(aSpec.subtype, "Button name has been specified");
+
+      nodeCollector.queryAnonymousNode("anonid", "buttons");
+      nodeCollector.root = nodeCollector.nodes[0];
+      nodeCollector.queryNodes("[dlgtype=" + aSpec.subtype +"]");
+      elems = nodeCollector.elements;
+      break;
+    case "save":
+      elems = [findElement.ID(root, "save")];
+      break;
+    case "remember":
+      elems = [findElement.ID(root, "rememberChoice")];
+      break;
+    default:
+      assert.fail("Unknown element type - " + aSpec.type);
+  }
+
+  return elems;
+}
+
+/**
+ * Helper for opening the Unknown Content Type dialog
+ *
+ * @param {function} aCallback
+ *        Callback that opens the unknownContentType dialog
+ *
+ * @returns {UnknownContentTypeDialog} New instance of UnknownContentTypeDialog
+ */
+function open(aCallback) {
+  assert.equal(typeof aCallback, "function", "Callback has been specified");
+
+  var controller = windows.waitForWindowState(() => {
+    aCallback();
+  }, {id: "unknownContentType", state: "open"});
+
+  return new UnknownContentTypeDialog(controller);
+}
+
+// Export of classes
+exports.UnknownContentTypeDialog = UnknownContentTypeDialog;
+
+// Export of methods
+exports.open = open;
+
