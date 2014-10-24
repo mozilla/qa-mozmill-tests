5df14b82f49b5c56c0648fd4b6bf5d96dc2d32e7
Daniel Gherasim <daniel.gherasim@softvision.ro>
2014-10-24 12:32:31 +0300

    Bug 1079710 - Add MenuPanel class in toolbars.js. r=aeftimie, r=amatei, r=hskupin
    --HG--
    branch : mozilla-aurora
    extra : transplant_source : %A3%88v%23%A0%E3W%9B%E36V%603%F0%C4m%3D%85x%23

---
+++ firefox/lib/tests/testMenuPanel.js
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// Include required modules
+var toolbars = require("../toolbars");
+
+var browser = require("../ui/browser");
+
+const MENU_PANEL_ELEMENTS = {
+  openButton: "toolbarbutton",
+  panel: "panel",
+  panel_addons: "toolbarbutton",
+  panel_fxaStatus: "toolbarbutton",
+  panel_newWindow: "toolbarbutton",
+  panel_preferences: "toolbarbutton",
+  panel_quitFirefox: "toolbarbutton"
+};
+
+function setupModule(aModule) {
+  aModule.browserWindow = new browser.BrowserWindow();
+  aModule.menuPanel = new toolbars.MenuPanel(aModule.browserWindow);
+}
+
+function teardownModule(aModule) {
+  aModule.menuPanel.close({force: true});
+}
+
+/**
+ * Bug 1079710
+ * Test that the elements in library are present on the Menu Panel
+ */
+function testMenuPanel() {
+  menuPanel.open();
+  expect.ok(menuPanel.isOpen, "Menu panel is open");
+
+  for (var element in MENU_PANEL_ELEMENTS) {
+    var el = menuPanel.getElement({type: element});
+    assert.equal(el.getNode().localName, MENU_PANEL_ELEMENTS[element],
+                 "Element has been found - " + element);
+  }
+
+  menuPanel.close();
+}
