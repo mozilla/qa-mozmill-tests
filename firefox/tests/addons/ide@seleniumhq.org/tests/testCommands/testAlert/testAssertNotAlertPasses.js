e7564c8cedf621b3f6332c4d12ca36e4065943ff
Cosmin Malutan <cosmin.malutan@softvision.ro>
2015-01-19 13:23:44 +0200

    Bug 1122530 - Call closeAllTabs before opening Selenium Manager. r=amatei

--- firefox/tests/addons/ide@seleniumhq.org/tests/testCommands/testAlert/testAssertNotAlertPasses.js
+++ firefox/tests/addons/ide@seleniumhq.org/tests/testCommands/testAlert/testAssertNotAlertPasses.js
@@ -11,11 +11,11 @@ var tabs = require("../../../../../../lib/tabs");
 
 function setupModule(aModule) {
   aModule.controller = mozmill.getBrowserController();
+  aModule.tabs.closeAllTabs(aModule.controller);
 
   aModule.sm = new selenium.SeleniumManager();
   aModule.sm.open(aModule.controller);
 
-  aModule.tabs.closeAllTabs(aModule.controller);
   aModule.controller.open("chrome://selenium-ide/content/tests/functional/aut/alert.html");
   aModule.controller.waitForPageLoad();
 }
