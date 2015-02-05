237d788d96cb1df7511c29f94345246afbadb4d5
Teodor Druta <teodor.druta@softvision.ro>
2015-02-05 16:47:45 +0200

    Bug 1071515 - Fixed test failure 'The Close button has not been found' in /testSecurity/testSafeBrowsingNotificationBar.js by maximizing the browser window r=amatei, r=mvelimiroviciu
    --HG--
    branch : mozilla-beta
    extra : transplant_source : %8F%09%E8%82%A80%EB%1C%CF%5CU%2CK%80%BE%EEH%5D%C4%C0

--- firefox/tests/remote/testSecurity/testSafeBrowsingNotificationBar.js
+++ firefox/tests/remote/testSecurity/testSafeBrowsingNotificationBar.js
@@ -33,12 +33,19 @@ var setupModule = function(aModule) {
 
   aModule.tabBrowser = new tabs.tabBrowser(aModule.controller);
 
+  // TODO:
+  // Bug 1084289 - The close button is not visible on certain locales and
+  // browser window sizes in the notification bar
+  aModule.browserWindow.maximize();
+
   aModule.tabBrowser.closeAllTabs();
 }
 
 function teardownModule(aModule) {
   // Clear the Safe Browsing permission
   utils.removePermission("www.itisatrap.org", "safe-browsing");
+
+  aModule.browserWindow.restore();
 }
 
 var testNotificationBar = function() {
