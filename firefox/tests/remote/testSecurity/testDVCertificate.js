17894456af4968fe35e6cef25fbf9d278ebfa230
Teodor Druta <teodor.druta@softvision.ro>
2015-02-12 11:03:03 +0200

    Bug 1027512 - Refactored getCertificate() security's lib function, in order to fix 'SSLStatus is null' js type error in security tests r=amatei, r=mvelimiroviciu, r=hskupin

--- firefox/tests/remote/testSecurity/testDVCertificate.js
+++ firefox/tests/remote/testSecurity/testDVCertificate.js
@@ -8,6 +8,7 @@ Cu.import("resource://gre/modules/Services.jsm");
 
 // Include necessary modules
 var { assert, expect } = require("../../../../lib/assertions");
+var security = require("../../../lib/security");
 var utils = require("../../../../lib/utils");
 var windows = require("../../../../lib/windows");
 
@@ -40,8 +41,7 @@ function testLarryBlue() {
   controller.waitForPageLoad();
 
   // Get the information from the certificate
-  var securityUI = controller.window.getBrowser().mCurrentBrowser.securityUI;
-  cert = securityUI.QueryInterface(Ci.nsISSLStatusProvider).SSLStatus.serverCert;
+  cert = security.getCertificate(browserWindow.tabs.securityUI);
 
   // Check the favicon
   var favicon = locationBar.getElement({type: "favicon"});
